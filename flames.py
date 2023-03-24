14457694c8b2dfea0da2c7eb3e3f4ba5cb9174d8
Jerlin J R <80874595+JerlinJR@users.noreply.github.com>
2023-03-24 22:49:29 +0530

    Gender Check

--- flames.py
+++ flames.py
@@ -1,59 +1,89 @@
-from operator import truediv
-import re
 import sys
+import maskpass
 
-boyName = input("Enter your name : ")
-girlName = input("Enter your partner name : ")
-
-# boyName = "nisha"
-# girlName = "admit"
-
-
-def innerRegex(boy,girl):
-    x=[]
-    name = boy + girl
-    for i in name:
-        if re.findall("[a-zA-Z]",i):
-            x.append(1)
-        else:
-            x.append(0)
-    x.sort(reverse = False)
-    if x[0] == 0:
-        print("Avoid Using Symbols or Numbers")
-        sys.exit()
-    else:
-        return True
-
-
-def outerRegex(boy,girl):
-    if boy == girl:
-        print("Please Give Two Different Name")
-        sys.exit()
-    elif innerRegex(boy,girl):
-        return True
-    else:
-        sys.exit()
-
-if outerRegex(boyName,girlName):
-    for i in boyName:
-        for j in girlName:
-            if i == j:
-                boyName = boyName.replace(i,"",1)
-                girlName = girlName.replace(j,"",1)
-                break
+def die(message):
+    sys.exit(message)
     
-num =  len(boyName+girlName)
-
-if num > 0:
-    flames = ["Friends","Lover","Affectionate","Marriage","Enemy","Sibling"]
-    while len(flames) > 1:
-        formula = num%len(flames)
-        removeIndex = formula - 1
-        if removeIndex >= 0:
-            left = flames[:removeIndex]
-            right = flames[removeIndex+1:]
-            flames = right+left
+def genderCheckGirl(name):
+        if len(name) > 0 and name[len(name) - 1] == 'a' or name[len(name) - 1] == 'i' or name[len(name) - 1] == 'y':
+            return name
         else:
-            flames = flames[:len(flames)-1]
+            die("Please Enter Girl Name Properly")
+            
+def genderCheckBoy(name):
+        if len(name) > 0 and name[len(name) - 1] == 'a' or name[len(name) - 1] == 'i': 
+            die("Please Enter Boy Name Correctly")
+        else:
+            return name
+            
+def remove_match_char(list1, list2):
 
-    print("RelationShip Between You and your loved one is: {}".format(flames[0]))
+	for i in range(len(list1)):
+		for j in range(len(list2)):
+
+			if list1[i] == list2[j]:
+				c = list1[i]
+
+				list1.remove(c)
+				list2.remove(c)
+
+				list3 = list1 + ["*"] + list2
+
+				return [list3, True]
+
+	list3 = list1 + ["*"] + list2
+	return [list3, False]
+
+
+# Driver code
+if __name__ == "__main__":
+
+	Boy = input("Boy Name : ")
+
+	Boy = genderCheckBoy(Boy.lower())
+
+	Boy.replace(" ", "")
+
+	Boy_list = list(Boy)
+
+	#Girl = input("Girl Name : ")
+	Girl = maskpass.askpass(prompt="Girl Name:", mask="❤")
+   
+	Girl = genderCheckGirl(Girl.lower())
+	Girl.replace(" ", "")
+	Girl_list = list(Girl)
+	proceed = True
+
+	while proceed:
+
+		ret_list = remove_match_char(Boy_list, Girl_list)
+
+		con_list = ret_list[0]
+
+		proceed = ret_list[1]
+
+		star_index = con_list.index("*")
+
+		Boy_list = con_list[: star_index]
+
+		Girl_list = con_list[star_index + 1:]
+
+	count = len(Boy_list) + len(Girl_list)
+
+	result = ["Friends", "Love", "Affection", "Marriage", "Enemy", "Siblings"]
+
+	while len(result) > 1:
+
+		split_index = (count % len(result) - 1)
+  
+		if split_index >= 0:
+
+			right = result[split_index + 1:]
+			left = result[: split_index]
+
+			result = right + left
+
+		else:
+			result = result[: len(result) - 1]
+
+	print("Relationship status :", result[0])
